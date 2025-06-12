a5e5c5e0fd9f6d84c361360ae2e8744fd43a8a6d
Alex Berger <alexbigkid@users.noreply.github.com>
2025-06-12 11:23:45 -0700

    t --version debug

--- src/eir/clo.py
+++ src/eir/clo.py
@@ -64,7 +64,10 @@ class CommandLineOptions:
         self.options = parser.parse_args()
 
         if self.options.version:
-            print(f"{CONST.NAME} version: {CONST.VERSION}", flush=True)
+            # Simple test bypassing constants
+            print("eir version: 0.1.47", flush=True)
+            print("This is a test output", flush=True)
+            sys.stdout.flush()
             sys.exit(0)
 
         if self.options.about:
