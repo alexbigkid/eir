5e3a84a23ae74b00fff03a7e68bb6bd5f90d6e52
Alex Berger <alexbigkid@users.noreply.github.com>
2025-06-21 21:24:11 -0700

    t trying to fix MacOS homebrew package validation test

--- homebrew/eir.rb
+++ homebrew/eir.rb
@@ -2,16 +2,8 @@ class Eir < Formula
   desc "EXIF-based image renamer and RAW format converter"
   homepage "https://github.com/alexbigkid/eir"
   version "REPLACE_WITH_VERSION"
-
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/alexbigkid/eir/releases/download/v#{version}/eir-#{version}-macos-universal"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    else
-      url "https://github.com/alexbigkid/eir/releases/download/v#{version}/eir-#{version}-macos-universal"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    end
-  end
+  url "https://github.com/alexbigkid/eir/releases/download/v#{version}/eir-#{version}-macos-universal"
+  sha256 "REPLACE_WITH_ACTUAL_SHA256"
 
   def install
     bin.install "eir-#{version}-macos-universal" => "eir"
